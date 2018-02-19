9886e034b10b874ba3546314e786124c45cc2797
Julian David Torregrosa Simbaqueva <PIIP41@dnp.gov.co>
2018-02-19 11:54:14 -0500

    Add heores

---
+++ praxis/src/data/heroes.js
@@ -0,0 +1,28 @@
+const heroes = [
+    {
+        id: 0,
+        name: 'Spider-Man',
+        history: 'is a fictional superhero appearing in American comic books published by Marvel Comics. The character was created by writer-editor Stan Lee and writer-artist Steve Ditko, and first appeared in the anthology comic book Amazing Fantasy #15 (August 1962) in the Silver Age of Comic Books. The character is conceived as an orphan within the Marvel Universe named Peter Parker being raised by his Aunt May and Uncle Ben in New York City after his parents Richard and Mary Parker were killed in a plane crash. Lee and Ditko depicted the character as having to deal with the normal struggles of adolescence and financial issues with a large array of supporting characters such as J. Jonah Jameson, Daily Bugle editor and smear campaigner of Spider-Man and classmates such as Flash Thompson, Harry Osborn and romantic interests, Gwen Stacy and Mary Jane Watson. His origin story depicts him as being bitten by a radioactive spider and thus acquiring spider-related power and abilities, such as the ability to cling to most surfaces, shoot spider-webs using wrist-mounted devices of his own invention, which he calls "web-shooters", and react to danger quickly with his "spider-sense", enabling him to combat his many superpowered foes, such as Doctor Octopus, Green Goblin and Venom. Also within the origin story, Spider-Man originally uses this power for stardom, but after letting a burglar escape who is responsible for shooting his uncle, he learns to use his power responsibly.',
+        image: 'https://en.wikipedia.org/wiki/Spider-Man#/media/File:Web_of_Spider-Man_Vol_1_129-1.png',
+        cover: 'https://img-cache.coolshop.com/9d604882-0607-4a19-b763-75f1687199d1/9d604882-0607-4a19-b763-75f1687199d1.jpg',
+        link: 'https://en.wikipedia.org/wiki/Spider-Man',
+        comics: [
+            { id: 0, title: 'Amazing Fantasy #15', year: '1962', image: 'https://en.wikipedia.org/wiki/Spider-Man#/media/File:Amazing_Fantasy_15.jpg' },
+            { id: 1, title: 'The Amazing Spider-Man #252', year: '1984', image: 'https://en.wikipedia.org/wiki/Spider-Man#/media/File:Issue252.jpg' },
+            { id: 2, title: 'The Amazing Spider-Man #121', year: '1980', image: 'https://en.wikipedia.org/wiki/Spider-Man#/media/File:Asm121.jpg' }
+        ]
+    },
+    {
+        id: 1,
+        name: 'Wolverine',
+        history: 'Wolverine (born James Howlett[1] commonly known as Logan and sometimes as Weapon X) is a fictional character appearing in American comic books published by Marvel Comics, mostly in association with the X-Men. He is a mutant who possesses animal-keen senses, enhanced physical capabilities, powerful regenerative ability known as a healing factor, and three retractable claws in each hand. Wolverine has been depicted variously as a member of the X-Men, Alpha Flight, and the Avengers. The character appeared in the last panel of The Incredible Hulk #180 before having a larger role in #181 (cover-dated Nov. 1974). He was created by Marvel editor-in-chief Roy Thomas,[2] writer Len Wein,[3] and Marvel art director John Romita, Sr. Romita designed the character, although it was first drawn for publication by Herb Trimpe. Wolverine then joined a revamped version of the superhero team the X-Men, where eventually writer Chris Claremont and artist-writer John Byrne would play significant roles in the character\'s development. Artist Frank Miller collaborated with Claremont and helped revise the character with a four-part eponymous limited series from September to December 1982, which debuted Wolverine\'s catchphrase, "I\'m the best there is at what I do, but what I do best isn\'t very nice."',
+        image: 'https://en.wikipedia.org/wiki/Wolverine_(character)#/media/File:Marvelwolverine.jpg',
+        cover: 'https://i.ytimg.com/vi/LWResYg3KCA/maxresdefault.jpg',
+        link: 'https://en.wikipedia.org/wiki/Wolverine_(character)',
+        comics: [
+            { id: 0, title: 'The Incredible Hulk #181', year: '1974', image: 'https://en.wikipedia.org/wiki/Wolverine_(character)#/media/File:Inchulk181.jpg' }
+        ]
+    }
+];
+
+export default heroes;
